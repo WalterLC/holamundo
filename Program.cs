9a4c1e95bd197748deed4d9e1f6b598218830995
WalterLC <waleoch@gmail.com>
2021-07-03 19:38:04 -0600

    Versión 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero=20;
+            int numero2=15;
+            int suma=0;
+            //string nombre=" ";
+           // Console.WriteLine("Escriba su nombre"); // mando por pantalla
+            //nombre = Console.ReadLine();  // capturo de la pantalla
+            
+            //Console.WriteLine("Escriba su edad");
+            //string num = Console.ReadLine();// me sirve para capturar numeros teclado
+            //numero = int.Parse(num);  // convierte de string a entero
+            //Console.WriteLine("Su nombre es: "+nombre);
+            //Console.WriteLine(numero);
+            suma= Suma(numero,numero2);
+            Console.WriteLine(suma);
+
+        }
+
+        static int Suma(int n, int n2){
+            return n+n2;
+
+        }
+
+
+    }
+}
